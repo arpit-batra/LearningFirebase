c696fbef437bdeea26e8650f9d169b31f23126f2
Appy <arpitbatra98@gmail.com>
2019-04-21 13:16:57 +0530

    Added Logout Option

--- app/src/main/java/com/example/setupfirebase/home.java
+++ app/src/main/java/com/example/setupfirebase/home.java
@@ -1,10 +1,32 @@
 package com.example.setupfirebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class home extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.home_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        FirebaseAuth.getInstance().signOut();
+        Intent intent=new Intent(getApplicationContext(),signup.class);
+        startActivity(intent);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
